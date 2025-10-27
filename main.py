138ab16235e3063c42c671f9807efa83dc5131d1
Joaquin Polonuer <jtpolonuer@gmail.com>
2025-10-26 20:34:11 -0400

    parametrized experiment

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ from src.experiments.pass_whole_context.longmemeval_experiment import run_experi
 load_dotenv()
 
 if __name__ == "__main__":
-    run_experiment(N=10)
+    run_experiment(
+        config={
+            "memory_model_type": "transformers",
+            "memory_model_name": "Qwen/Qwen3-4B",
+            "memory_model_quantized": True,
+            "judge_model_name": "azure/gpt-4.1",
+            "longmemeval_dataset_type": "oracle",
+            "N": 10,
+        }
+    )
--- src/experiments/pass_whole_context/longmemeval_experiment.py
+++ src/experiments/pass_whole_context/longmemeval_experiment.py
@@ -5,16 +5,24 @@ from src.core.LongMemEvalDataset import LongMemEvalDataset
 from src.models.QwenModel import QwenModel
 
 
-def run_experiment(N: int):
-    # litellm_model = LiteLLMModel("azure/gpt-4.1")
-    model = QwenModel("Qwen/Qwen3-4B")
-    judge_agent = JudgeAgent(model)
-    memory_agent = FullContextAgent(model)
+def run_experiment(config):
+    if config["memory_model_type"] == "transformers":
+        memory_model = QwenModel(
+            config["memory_model_name"], quantized=config["memory_model_quantized"]
+        )
+    elif config["memory_model_type"] == "litellm":
+        memory_model = LiteLLMModel(config["memory_model_name"])
+    else:
+        raise ValueError(f"Invalid model type: {config['memory_model_type']}")
+
+    judge_model = LiteLLMModel(config["judge_model_name"])
+    judge_agent = JudgeAgent(model=judge_model)
+    memory_agent = FullContextAgent(model=memory_model)
 
     correct_predictions = 0
-    longmemeval_o_dataset = LongMemEvalDataset("short")
+    longmemeval_o_dataset = LongMemEvalDataset(config["longmemeval_dataset_type"])
 
-    for question, sessions, t_question, answer in longmemeval_o_dataset[:N]:
+    for question, sessions, t_question, answer in longmemeval_o_dataset[: config["N"]]:
         predicted_answer = memory_agent.answer(sessions, question, t_question)
         answer_is_correct = judge_agent.judge(question, predicted_answer, answer)
         if answer_is_correct:
