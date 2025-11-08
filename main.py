da88375b28b9fe8a77ced30dc1b6558392fe8212
Joaquin Polonuer <jtpolonuer@gmail.com>
2025-11-08 15:17:13 -0300

    other configurations

--- main.py
+++ main.py
@@ -17,3 +17,25 @@ if __name__ == "__main__":
         N=-1,
     )
     run_experiment(config)
+
+
+# Otros ejemplos de configuracion
+# Si pasamos esta MemoryModelConfig, vamos a usar un modelo de transformers de huggingface.
+# MemoryModelConfig(
+#     model_type="transformers",
+#     model_name="Qwen/Qwen3-4B",
+# ),
+# Para que esto funcione, necesitan correrlo en una maquin acorde.
+
+# Tambien podemos usar un modelo con mucho contexto para probar.
+# Otro detalle, para los modelos de openai, se puede especificar el proveedor.
+# config = Config(
+#     memory_model=MemoryModelConfig(
+#         model_type="litellm",
+#         model_name="openai/gpt-5",
+#     ),
+#     memory_agent="RAG",
+#     judge_model_name="openai/gpt-5",
+#     longmemeval_dataset_type="short",
+#     N=-1,
+# )
